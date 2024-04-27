453855c6ed7463e30d9791482b384c4cd2cb8bda
dhaneshwar111 <dhaneshwarkumar435@gmail.com>
2024-04-27 14:44:12 +0530

    portfolio

--- script.js
+++ script.js
@@ -15,34 +15,16 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Fronted",  "Developer", "Fresher"],
+        strings: ["Fresher", "Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Fronted",  "Developer", "Fresher"],
+        strings: ["Fresher", "Front-end Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
